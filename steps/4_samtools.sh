17b4dd5d9fe96aec47b285c478bbb73eda026452
Oliver Tessmer <oliver.tessmer@gmail.com>
2023-08-15 15:52:19 -0400

    added loops to steps 4 and 5

--- steps/4_samtools.sh
+++ steps/4_samtools.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/env bash
 
-samtools view -h -F 4 /mnt/output/aligned.sam | grep -v "XS:i" > /mnt/output/4a.sam
-samtools view -h -Sb /mnt/output/4a.sam -o /mnt/output/4b.bam
-samtools sort /mnt/output/4b.bam -o /mnt/output/unique.bam
-samtools index /mnt/output/unique.bam'
+# list of files to extract uniquely mapped reads from
+# all files in /mnt/output (aligned sams created in step #3)
+specs=(
+    "P1_R1_B73_aligned.sam"
+    "P1_R2_B73_aligned.sam"
+    "Halo_R1_B73_aligned.sam"
+    "Halo_R2_B73_aligned.sam"
+    "P1_R1_A632_aligned.sam"
+    "P1_R2_A632_aligned.sam"
+    "Halo_R1_A632_aligned.sam"
+    "Halo_R2_A632_aligned.sam"
+)
+
+for fname in "${specs[@]}"; do
+    echo "samtools "$fname;
+
+    samtools view -h -F 4 /mnt/output/$fname | grep -v "XS:i" > "/mnt/output/"$fname".4a.sam"
+    samtools view -h -Sb "/mnt/output/"$fname".4a.sam" -o "/mnt/output/"$fname".4b.bam"
+    samtools sort "/mnt/output/"$fname".4b.bam" -o "/mnt/output/"$fname".unique.bam"
+    samtools index "/mnt/output/"$fname".unique.bam"
+done
--- steps/5_samtools.sh
+++ steps/5_samtools.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 
-samtools view -q 42 -b /mnt/output/aligned.sam -o /mnt/output/5a.bam
-samtools sort /mnt/output/5a.bam -o /mnt/output/perfect.bam
-samtools index /mnt/output/perfect.bam
+# list of files from which to extract uniquely mapped reads with perfect math
+# all files in /mnt/output (aligned sams created in step #3)
+specs=(
+    "P1_R1_B73_aligned.sam"
+    "P1_R2_B73_aligned.sam"
+    "Halo_R1_B73_aligned.sam"
+    "Halo_R2_B73_aligned.sam"
+    "P1_R1_A632_aligned.sam"
+    "P1_R2_A632_aligned.sam"
+    "Halo_R1_A632_aligned.sam"
+    "Halo_R2_A632_aligned.sam"
+)
+
+for fname in "${specs[@]}"; do
+    echo "samtools "$fname;
+
+    samtools view -q 42 -b /mnt/output/$fname -o "/mnt/output/"$fname".5a.bam"
+    samtools sort "/mnt/output/"$fname".5a.bam" -o "/mnt/output/"$fname".perfect.bam"
+    samtools index "/mnt/output/"$fname".perfect.bam"
+
+done
