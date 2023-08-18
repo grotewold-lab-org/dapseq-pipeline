670628cfe9ab5ab1f421ddd38127446836e7ccc4
tessmero <grotewoldlab@gmail.com>
2023-08-18 13:46:07 -0400

    test just first step in CI

--- run_all_steps.sh
+++ run_all_steps.sh
@@ -3,19 +3,19 @@
 docker compose run 1_trim
 
 # bowtie2 genome index (ref genome -> index)
-docker compose run 2_bt2-build
+#docker compose run 2_bt2-build
 
 # bowtie2 mapping (clean fasta + index -> sam)
-docker compose run 3_bt2-map
+#docker compose run 3_bt2-map
 
 # extract uniquely mapped reads (sam -> bam)
-docker compose run 4_samtools
+#docker compose run 4_samtools
 
 # extract uniquely mapped reads with perfect match (MAPQ= 42)
-docker compose run 5_samtools
+#docker compose run 5_samtools
 
 # GEM tool (bams -> peaks)
-docker compose run 6_gemtool
+#docker compose run 6_gemtool
 
 # IDR (peaks -> high confidence peaks)
-docker compose run 7_idr
+#docker compose run 7_idr
