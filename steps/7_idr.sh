d8edbd6deed1f9b718d5959b85dc65d75d30c6ab
Oliver Tessmer <oliver.tessmer@gmail.com>
2023-08-18 12:36:44 -0400

    fixed step7 (IDR) and file permissions

--- steps/7_idr.sh
+++ steps/7_idr.sh
@@ -4,16 +4,16 @@
 # all files in /mnt/output (peaks created by gemtools in step #6)
 
 idr --samples \
-    /mnt/output/R1_B73_peaks/R1_B73_peaks.GPS_events.narrowPeak \
-    /mnt/output/R2_B73_peaks/R2_B73_peaks.GPS_events.narrowPeak \
+    /mnt/output/R1_B73_peaks/R1_B73_peaks.GEM_events.narrowPeak \
+    /mnt/output/R2_B73_peaks/R2_B73_peaks.GEM_events.narrowPeak \
     --input-file-type narrowPeak \
     --output-file /mnt/output/B73_idr_peaks.out \
     --plot
 
 
 idr --samples \
-    /mnt/output/R1_A632_peaks/R1_A632_peaks.GPS_events.narrowPeak \
-    /mnt/output/R2_A632_peaks/R2_A632_peaks.GPS_events.narrowPeak \
+    /mnt/output/R1_A632_peaks/R1_A632_peaks.GEM_events.narrowPeak \
+    /mnt/output/R2_A632_peaks/R2_A632_peaks.GEM_events.narrowPeak \
     --input-file-type narrowPeak \
     --output-file /mnt/output/A632_idr_peaks.out \
     --plot
